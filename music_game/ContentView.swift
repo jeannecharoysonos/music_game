dad2c8be17e2212f2412292b1c0cee1ee2bb0584
jeannecharoysonos <jeanne.charoy@sonos.com>
2023-09-15 15:30:57 +0200

    Initial Commit

---
+++ music_game/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  music_game
+//
+//  Created by Jeanne Charoy on 15/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ music_game/music_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_gameApp.swift
+//  music_game
+//
+//  Created by Jeanne Charoy on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct music_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
